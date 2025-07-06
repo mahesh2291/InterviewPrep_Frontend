530f45d88003073ca539fd5e880be887bfbeffa5
mahesh2291 <gmahesh252525@gmail.com>
2025-07-06 17:39:37 -0400

    fixed api path

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,26 +1,25 @@
-export const BASE_URL=location.hostname==="localhost"? "http://localhost:3000" : "/api"
- 
-export const API_PATHS={
-    AUTH:{
-        REGISTER: "api/auth/register",
-        LOGIN:"api/auth/login",
-        GET_PROFILE:"api/auth/profile"
-    },
-    SESSIONS:{
-        GET_ALL:"api/sessions/my-sessions",
-        CREATE:"api/sessions/create",
-        GET_BY_ID:(id)=>`api/sessions/${id}`,
-        DELETE:(id)=>`api/sessions/${id}`
-    },
-    AI:{
-        GENERATE_QUESTIONS:'/api/ai/generate-questions',
-        GENERATE_EXPLAINATION:'/api/ai/generate-explaination'
-    },
-    TOGGLE_QUESTION:{
-        PIN:(id)=>`api/questions/${id}/pin`
-    },
-    QUESTION:{
-        ADD_TO_SESSION:'api/questions/add'
-    },
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 
-}
+export const API_PATHS = {
+  AUTH: {
+    REGISTER: "auth/register",
+    LOGIN: "auth/login",
+    GET_PROFILE: "auth/profile",
+  },
+  SESSIONS: {
+    GET_ALL: "sessions/my-sessions",
+    CREATE: "sessions/create",
+    GET_BY_ID: (id) => `sessions/${id}`,
+    DELETE: (id) => `sessions/${id}`,
+  },
+  AI: {
+    GENERATE_QUESTIONS: "ai/generate-questions",
+    GENERATE_EXPLAINATION: "ai/generate-explaination",
+  },
+  TOGGLE_QUESTION: {
+    PIN: (id) => `questions/${id}/pin`,
+  },
+  QUESTION: {
+    ADD_TO_SESSION: "questions/add",
+  },
+};
